4f7fdf322a2b6c9e5ebb56f163a615f00e5b7dee
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2024-02-18 16:35:05 +0100

    Update RealEstate.sol

--- Real Estate/millow/contracts/RealEstate.sol
+++ Real Estate/millow/contracts/RealEstate.sol
@@ -1,13 +1,14 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract RealEstate is ERC721URIStorage {
+
+  contract RealEstate is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter  private _tokenIds;
 
     constructor() ERC721("Real Estate", "REAL") {}
 
